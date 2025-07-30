b6f210f61e57cb17dd59b0c821ec0bbdda48280c
Sadam <sadebola@myseneca.ca>
2025-07-30 16:20:09 -0400

    Initial error handler

---
+++ utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+OPS445 Assignment 2
+Program: assignment2.py 
+Author: Sadam Adebola
+'''
+
+import os
+import sys
+import time
+import subprocess
+from datetime import datetime
+
+def print_err(err):
+    print("Error:", err)
+    sys.exit(1)
