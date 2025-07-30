05b62d1a3baaea4b495a9fe52959ac8db479c71e
Sadam <sadebola@myseneca.ca>
2025-07-30 16:24:40 -0400

    User input and validation

--- utils.py
+++ utils.py
@@ -15,3 +15,11 @@ from datetime import datetime
 def print_err(err):
     print("Error:", err)
     sys.exit(1)
+
+def get_input_user():
+    userid = input("Enter userid: ")
+    if check_user_in_sys(userid):
+        return userid
+    else:
+        print_err("Invalid userid!")
+    return
