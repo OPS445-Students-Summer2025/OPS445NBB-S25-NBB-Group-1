e2372ccae933f4a6966af5ee450d4d82b2e28db9
An Truong <datruong@myseneca.ca>
2025-07-30 12:48:22 -0400

    add main block

--- userlogin.py
+++ userlogin.py
@@ -34,22 +34,22 @@ def write_log(userid, indate,login_time, logout_time,duration,remark):
     except:
         print_err("Failed to write log!")
 
-# if __name__ == "__main__":
-#     print("Select an option:")
-#     print("1 - Track user")
-#     print("2 - Print report")
-#     print("3 - Add/delete user")    
-#     print("9 - Exit")
+if __name__ == "__main__":
+    print("Select an option:")
+    print("1 - Track user")
+    print("2 - Print report")
+    print("3 - Add/delete user")    
+    print("9 - Exit")
 
-#     choice = input("Enter your choice: ")
+    choice = input("Enter your choice: ")
 
-#     if choice == '1':
-#         track_user()
-#     elif choice == '2':
-#         print_report()            
-#     elif choice == '9':
-#         sys.exit(0)
-#     else:
-#         print("Invalid choice. Please try again.")
+    if choice == '1':
+        track_user()
+    elif choice == '2':
+        print_report()            
+    elif choice == '9':
+        sys.exit(0)
+    else:
+        print("Invalid choice. Please try again.")
 
 
